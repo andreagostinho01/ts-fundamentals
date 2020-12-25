56ee0e8efadfb96f4a541e873e63b79ecd41a2f0
andre <andre.carlos.agostinho@gmail.com>
2020-12-25 15:18:17 -0300

    lesson 3

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,3 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 20;
-character = 'luigi';
-// age = 'yoshi';
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var area = function (diameter) {
-    return diameter * Math.PI;
-};
-// console.log(area('hello'));
-console.log(area(7.5));
+var randomNumber = 14;
+var times2 = function (num) { return num * 2; };
+console.log(times2(randomNumber));
--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,4 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+const randomNumber = 14;
 
-// character = 20;
-character = 'luigi';
-
-// age = 'yoshi';
-age = 40;
-
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
-};
-
-// console.log(area('hello'));
-console.log(circ(7.5));
+const times2 = (num: number) => num * 2;
+console.log(times2(randomNumber));
