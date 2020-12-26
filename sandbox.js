27adb461c8696a1c1e087f4cca1946a3cbbd30f3
andre <andre.carlos.agostinho@gmail.com>
2020-12-26 18:48:51 -0300

    lesson 6

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+// we can declare a variable that can be of any type
+var anyTypeVar;
+anyTypeVar = 2020;
+console.log(anyTypeVar);
+anyTypeVar = 'Random text';
+console.log(anyTypeVar);
+anyTypeVar = false;
+console.log(anyTypeVar);
+// same for arrays
+var mixedArr = [];
+mixedArr.push('Lorem ipsum');
+mixedArr.push(2050);
+mixedArr.push(true);
+console.log(mixedArr);
--- sandbox.ts
+++ sandbox.ts
@@ -1,1 +1,19 @@
+// we can declare a variable that can be of any type
+let anyTypeVar: any;
 
+anyTypeVar = 2020;
+console.log(anyTypeVar);
+
+anyTypeVar = 'Random text';
+console.log(anyTypeVar);
+
+anyTypeVar = false;
+console.log(anyTypeVar);
+
+// same for arrays
+let mixedArr: any[] = [];
+
+mixedArr.push('Lorem ipsum');
+mixedArr.push(2050);
+mixedArr.push(true);
+console.log(mixedArr);
