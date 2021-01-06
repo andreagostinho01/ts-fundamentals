c1781288056b4a91d285b0808592fb32f3490054
andre <andre.carlos.agostinho@gmail.com>
2021-01-06 19:43:15 -0300

    lesson 19

--- public/app.js
+++ public/app.js
@@ -1,45 +1,41 @@
-import { Invoice } from './classes/Invoice.js';
-import { Payment } from './classes/Payment.js';
-import { ListTemplate } from './classes/ListTemplate.js';
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-// inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-// list template instance
-const ul = document.querySelector('ul');
-const list = new ListTemplate(ul);
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let doc;
-    if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
-    }
-    else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
-    }
-    list.render(doc, type.value, 'end');
-});
-// ENUMS
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
-    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
-    ResourceType[ResourceType["FILM"] = 2] = "FILM";
-    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
-})(ResourceType || (ResourceType = {}));
-;
-const docOne = {
-    uid: 1,
-    resourceType: ResourceType.BOOK,
-    data: { title: 'name of the wind' }
+"use strict";
+function addUUID(obj) {
+    const uuid = 'd97a4534-1cd0-40b5-80aa-b2a1b868bf0b';
+    return Object.assign({ uuid }, obj);
+}
+const userWithUUID = addUUID({ name: 'André', age: 19 });
+console.log(userWithUUID);
+/*
+  the next line throws an error because TS can't see there's a name property;
+  to solve this we need to use Generics;
+  Generics referece: https://www.typescriptlang.org/docs/handbook/generics.html
+*/
+// console.log(userWithUUID.name); // error
+// passed parameter must be an object
+function addUUIDAgain(obj) {
+    const uuid = '4831461a-2445-40a8-9cbb-9f1706aeb09c';
+    return Object.assign({ uuid }, obj);
+}
+const anotherUserWithUUID = addUUIDAgain({ name: 'Fernando', age: 35 });
+console.log(anotherUserWithUUID);
+console.log(anotherUserWithUUID.name);
+console.log('==================================');
+function logCity(city) {
+    console.log(city);
+    return true;
+}
+logCity({ city: 'Brotas' });
+logCity({ city: 'Campinas', state: 'São Paulo' });
+const hotDog = {
+    name: 'Hot dog',
+    data: 'some information',
 };
-const docTwo = {
-    uid: 10,
-    resourceType: ResourceType.DIRECTOR,
-    data: { title: 'name of the wind' }
+const pizza = {
+    name: 'Pizza',
+    data: {
+        ingredients: ['cheese', 'pepperoni', 'tomato sauce'],
+        price: 30
+    }
 };
-console.log(docOne);
-console.log(docTwo);
+console.log(hotDog);
+console.log(pizza);
--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,55 @@
-import { Invoice } from './classes/Invoice.js';
-import { Payment } from './classes/Payment.js';
-import { ListTemplate } from './classes/ListTemplate.js';
-import { HasFormatter } from './interfaces/HasFormatter.js';
+function addUUID(obj: object) {
+  const uuid = 'd97a4534-1cd0-40b5-80aa-b2a1b868bf0b';
+  return { uuid, ...obj };
+}
 
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
+const userWithUUID = addUUID({ name: 'André', age: 19 });
+console.log(userWithUUID);
 
-// inputs
-const type = document.querySelector('#type') as HTMLInputElement;
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+/* 
+  the next line throws an error because TS can't see there's a name property;
+  to solve this we need to use Generics;
+  Generics referece: https://www.typescriptlang.org/docs/handbook/generics.html
+*/
+// console.log(userWithUUID.name); // error
 
-// list template instance
-const ul = document.querySelector('ul')!;
-const list = new ListTemplate(ul);
+// passed parameter must be an object
+function addUUIDAgain <T extends object> (obj: T) {
+  const uuid = '4831461a-2445-40a8-9cbb-9f1706aeb09c';
+  return { uuid, ...obj };
+}
 
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault();
+const anotherUserWithUUID = addUUIDAgain({ name: 'Fernando', age: 35 });
+console.log(anotherUserWithUUID);
+console.log(anotherUserWithUUID.name);
 
-  let doc: HasFormatter;
-  if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
-  } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+console.log('==================================');
+
+function logCity <T extends { city: string }> (city: T) {
+  console.log(city);
+  return true;
+}
+
+logCity({ city: 'Brotas' });
+logCity({ city: 'Campinas', state: 'São Paulo' });
+
+interface Food<T> {
+  name: string,
+  data: T
+}
+
+const hotDog: Food<string> = {
+  name: 'Hot dog',
+  data: 'some information',
+};
+
+const pizza: Food<object> = {
+  name: 'Pizza',
+  data: {
+    ingredients: ['cheese', 'pepperoni', 'tomato sauce'],
+    price: 30
   }
-  
-  list.render(doc, type.value, 'end');
-});
+};
 
-// ENUMS
-
-enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR };
-
-interface Resource<T> {
-  uid: number;
-  resourceType: ResourceType;
-  data: T;
-}
-
-const docOne: Resource<object> = {
-  uid: 1,
-  resourceType: ResourceType.BOOK,
-  data: { title: 'name of the wind' }
-}
-const docTwo: Resource<object> = {
-  uid: 10,
-  resourceType: ResourceType.DIRECTOR,
-  data: { title: 'name of the wind' }
-}
-
-console.log(docOne);
-console.log(docTwo);
+console.log(hotDog);
+console.log(pizza);
